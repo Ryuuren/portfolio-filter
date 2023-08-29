bd3476380dc8b9cd163bb33165ca4cda5cc5042c
Mitchell Patch <111026568+Ryuuren@users.noreply.github.com>
2023-08-29 21:59:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,192 +1,45 @@
-// Waits for the DOM to be fully loaded before executing any JavaScript
+// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-  // Assigns variables to the important HTML elements & containers
-  const tags = document.querySelectorAll(".tag");
-  const cardContainer = document.querySelector(".card-container");
-  const tagCheckboxes = document.querySelectorAll(".tag-checkbox");
-  // Assigns variables to the conditional checkboxes that appear once the primary checkboxes are checked
-  const conditionalParentWrapper = document.querySelector(".conditional-parent-wrapper");
-  const websitesCheckboxes = document.querySelector(".websites-checkboxes");
-  const designCheckboxes = document.querySelector(".design-checkboxes");
-  const digitalMarketingCheckboxes = document.querySelector(".digital-marketing-checkboxes");
-  // Assigns all client cards in gallery to a variable
-  let cards = Array.from(document.querySelectorAll(".card"));
-  // Assigns variable to the searchbar input
-  const filterInput = document.querySelector("#filterInput");
-  // Assigns variable to the show all checkbox
-  const showAllCheckbox = document.querySelector('input[value="all"]');
-
-  // Event Listener function for clicking show all and resetting the filter system
-  showAllCheckbox.addEventListener("change", function () {
-    if (this.checked) {
-      tagCheckboxes.forEach(function (checkbox) {
-        checkbox.checked = false;
-      });
-
-      const conditionalCheckboxes = document.querySelectorAll(".conditional-checkbox");
-      conditionalCheckboxes.forEach(function (checkbox) {
-        checkbox.checked = false;
-      });
-    }
-  });
-
-  tagCheckboxes.forEach(function (checkbox) {
-    checkbox.addEventListener("change", function () {
-      if (checkbox.value === "all" && checkbox.checked) {
-        tagCheckboxes.forEach(function (cb) {
-          if (cb !== checkbox) {
-            cb.checked = false;
-          }
-        });
-      } else {
-        tagCheckboxes.forEach(function (cb) {
-          if (cb.value === "all") {
-            cb.checked = false;
-          }
-        });
-      }
-
-      const selectedPrimaryTags = Array.from(tagCheckboxes)
-        .filter(function (checkbox) {
-          return checkbox.checked && checkbox.value !== "all";
-        })
-        .map(function (checkbox) {
-          return checkbox.value;
-        });
-
-      conditionalParentWrapper.classList.toggle("hidden", selectedPrimaryTags.length === 0);
-      websitesCheckboxes.classList.toggle("hidden", !selectedPrimaryTags.includes("websites"));
-      designCheckboxes.classList.toggle("hidden", !selectedPrimaryTags.includes("design"));
-      digitalMarketingCheckboxes.classList.toggle(
-        "hidden",
-        !selectedPrimaryTags.includes("digital-marketing")
-      );
-
-      // Clear the checked state of conditional checkboxes when a primary tag checkbox is unchecked
-      if (!checkbox.checked) {
-        const conditionalCheckboxes =
-          conditionalParentWrapper.querySelectorAll(".conditional-checkbox");
-        conditionalCheckboxes.forEach(function (cb) {
-          cb.checked = false;
-        });
-      }
-
-      cards.forEach((card) => {
-        const cardTags = card.getAttribute("data-tags").split(" ");
-        const primaryTagsMatch = selectedPrimaryTags.every(function (tag) {
-          return cardTags.includes(tag);
-        });
-
-        // Collect all selected conditional checkboxes
-        const selectedConditionalCheckboxes = Array.from(
-          conditionalParentWrapper.querySelectorAll(".conditional-checkbox:checked")
-        );
-
-        // Extract the values of the selected conditional checkboxes
-        const selectedConditionalTags = selectedConditionalCheckboxes.map(function (checkbox) {
-          return checkbox.value;
-        });
-
-        const conditionalTagsMatch =
-          selectedConditionalTags.length === 0 ||
-          selectedConditionalTags.some(function (tag) {
-            return cardTags.includes(tag);
-          });
-
-        if (primaryTagsMatch && conditionalTagsMatch) {
-          card.classList.add("show");
-          card.classList.remove("hide");
-        } else {
-          card.classList.remove("show");
-          card.classList.add("hide");
-        }
-      });
-    });
-  });
-
-  // Event listener for conditional checkboxes
-  const conditionalCheckboxes = document.querySelectorAll(".conditional-checkbox");
-  conditionalCheckboxes.forEach(function (checkbox) {
-    checkbox.addEventListener("change", function () {
-      const selectedPrimaryTags = Array.from(tagCheckboxes)
-        .filter(function (checkbox) {
-          return checkbox.checked && checkbox.value !== "all";
-        })
-        .map(function (checkbox) {
-          return checkbox.value;
-        });
-
-      cards.forEach(function (card) {
-        const cardTags = card.getAttribute("data-tags").split(" ");
-        const primaryTagsMatch = selectedPrimaryTags.every(function (tag) {
-          return cardTags.includes(tag);
-        });
-
-        // Collect all selected conditional checkboxes
-        const selectedConditionalCheckboxes = Array.from(
-          conditionalParentWrapper.querySelectorAll(".conditional-checkbox:checked")
-        );
-
-        // Extract the values of the selected conditional checkboxes
-        const selectedConditionalTags = selectedConditionalCheckboxes.map(function (checkbox) {
-          return checkbox.value;
-        });
-
-        const conditionalTagsMatch =
-          selectedConditionalTags.length === 0 ||
-          selectedConditionalTags.some(function (tag) {
-            return cardTags.includes(tag);
-          });
-
-        if (primaryTagsMatch && conditionalTagsMatch) {
-          card.classList.add("show");
-          card.classList.remove("hide");
-        } else {
-          card.classList.remove("show");
-          card.classList.add("hide");
-        }
-      });
-    });
-  });
-
-  // Function to filter the cards based on search bar input
+  // Function to filter and display cards based on selected tags and checkboxes
   function filterCards() {
-    const filterValue = filterInput.value.toLowerCase();
-    const selectedPrimaryTags = Array.from(tagCheckboxes)
-      .filter(function (checkbox) {
-        return checkbox.checked && checkbox.value !== "all";
+    // Get active tag buttons
+    let activeTags = Array.from(document.querySelectorAll(".tag-button"))
+      .filter(function (tag) {
+        return tag.classList.contains("active");
       })
-      .map(function (checkbox) {
-        return checkbox.value;
+      .map(function (tag) {
+        return tag.dataset.value;
       });
 
+    // Get active conditional buttons for selected tags
+    let activeConditionalButtons = [];
+    activeTags.forEach(function (tag) {
+      let conditionalButtons = Array.from(
+        document.querySelectorAll("." + tag + "-checkbox")
+      ).filter(function (button) {
+        return button.classList.contains("active");
+      });
+      activeConditionalButtons = activeConditionalButtons.concat(conditionalButtons);
+    });
+
+    // Iterate through cards and show/hide them based on filters
     cards.forEach(function (card) {
-      const cardTags = card.getAttribute("data-tags").split(" ");
-      const primaryTagsMatch = selectedPrimaryTags.every(function (tag) {
-        return cardTags.includes(tag);
-      });
-
-      // Collect all selected conditional checkboxes
-      const selectedConditionalCheckboxes = Array.from(
-        conditionalParentWrapper.querySelectorAll(".conditional-checkbox:checked")
-      );
-
-      // Extract the values of the selected conditional checkboxes
-      const selectedConditionalTags = selectedConditionalCheckboxes.map(function (checkbox) {
-        return checkbox.value;
-      });
-
-      const conditionalTagsMatch =
-        selectedConditionalTags.length === 0 ||
-        selectedConditionalTags.some(function (tag) {
-          return cardTags.includes(tag);
+      let tags = card.getAttribute("data-tags").split(" "),
+        hasAllTags = activeTags.every(function (tag) {
+          return tags.includes(tag);
+        }),
+        hasAnyConditional = activeConditionalButtons.map(function (button) {
+          return button.dataset.value;
         });
-
-      if (
-        primaryTagsMatch &&
-        conditionalTagsMatch &&
-        card.textContent.toLowerCase().includes(filterValue)
-      ) {
+      hasAnyConditional =
+        0 === hasAnyConditional.length ||
+        hasAnyConditional.some(function (conditional) {
+          return tags.includes(conditional);
+        });
+      let showAllActive = document
+        .querySelector('button[data-value="all"]')
+        .classList.contains("active");
+      if ((hasAllTags && hasAnyConditional) || showAllActive) {
         card.classList.add("show");
         card.classList.remove("hide");
       } else {
@@ -196,12 +49,52 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  filterInput.addEventListener("input", filterCards);
+  // Get all tag buttons and cards
+  let tags = document.querySelectorAll(".tag-button"),
+    cards = Array.from(document.querySelectorAll(".card"));
 
-  // Resets the search bar when clear button is clicked
-  const btnClear = document.querySelector(".btn-clear");
-  btnClear.addEventListener("click", function () {
-    filterInput.value = "";
-    filterCards();
+  // Add click event listeners to tag buttons
+  tags.forEach(function (tag) {
+    tag.addEventListener("click", function () {
+      this.classList.toggle("active");
+      if (this.dataset.value === "all" && this.classList.contains("active")) {
+        tags.forEach(function (tag) {
+          tag !== this && tag.classList.remove("active");
+        }, this);
+        document.querySelectorAll(".conditional-button").forEach(function (button) {
+          button.classList.remove("active");
+        });
+        document.querySelectorAll(".conditional-button").forEach(function (button) {
+          button.classList.add("hidden");
+        });
+      } else {
+        document.querySelector('button[data-value="all"]').classList.remove("active");
+        if (this.classList.contains("active")) {
+          document
+            .querySelectorAll("." + this.dataset.value + "-checkbox")
+            .forEach(function (button) {
+              button.classList.remove("hidden");
+            });
+        } else {
+          document
+            .querySelectorAll("." + this.dataset.value + "-checkbox")
+            .forEach(function (button) {
+              button.classList.add("hidden");
+            });
+        }
+      }
+      filterCards();
+    });
   });
+
+  // Add click event listeners to conditional checkboxes
+  document.querySelectorAll(".conditional-button").forEach(function (button) {
+    button.addEventListener("click", function () {
+      this.classList.toggle("active");
+      filterCards();
+    });
+  });
+
+  // Initial call to filterCards to set initial state
+  filterCards();
 });
